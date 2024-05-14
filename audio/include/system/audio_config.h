bab7566aa7952cfcd2b324ee1acdb7b7e56b2c81
Shalini Manjunatha <quic_c_shalma@quicinc.com>
2024-05-14 11:17:27 +0530

    media: include customization folder for audio policy
    Include customization  folder vendor/etc/audio for audio policy
    
    Change-Id: I11f68c92f7b799c528ef0e6e00dd66d6349888bc
    (cherry picked from commit 791cc8a19f4143c3cd73b50c8dbc9548722709b7)

--- audio/include/system/audio_config.h
+++ audio/include/system/audio_config.h
@@ -13,6 +13,39 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+/*Changes from Qualcomm Innovation Center are provided under the following license:
+ *Copyright (c) 2024 Qualcomm Innovation Center, Inc. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted (subject to the limitations in the
+ * disclaimer below) provided that the following conditions are met:
+ *
+ *  * Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *
+ *   * Neither the name of Qualcomm Innovation Center, Inc. nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE
+ * GRANTED BY THIS LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT
+ * HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+ * GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+ * IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
+ * IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 
 #ifndef ANDROID_AUDIO_CONFIG_H
 #define ANDROID_AUDIO_CONFIG_H
@@ -33,11 +66,12 @@ static inline std::vector<std::string> audio_get_configuration_paths() {
     static const std::vector<std::string> paths = []() {
         char value[PROPERTY_VALUE_MAX] = {};
         if (property_get("ro.boot.product.vendor.sku", value, "") <= 0) {
-            return std::vector<std::string>({"/odm/etc", "/vendor/etc", "/system/etc"});
+            return std::vector<std::string>({"/odm/etc", "/vendor/etc/audio",
+                    "/vendor/etc", "/system/etc"});
         } else {
             return std::vector<std::string>({
                     "/odm/etc", std::string("/vendor/etc/audio/sku_") + value,
-                    "/vendor/etc", "/system/etc"});
+                    "/vendor/etc/audio", "/vendor/etc", "/system/etc"});
         }
     }();
     return paths;
