7721f2130ae1fe1c568982887c4761bbaf953344
Aravind Siddappaji <asiddapp@qti.qualcomm.com>
2026-03-03 11:44:51 +0800

    media: include customization folder for audio policy
    Include customization  folder vendor/etc/audio for audio policy
    
    Change-Id: Ifaadd78f1729636ef87e6e6d3d6eb8d2d3aa08b8

--- audio/include/system/audio_config.h
+++ audio/include/system/audio_config.h
@@ -67,11 +67,12 @@ static inline std::vector<std::string> audio_get_configuration_paths() {
         char value[PROPERTY_VALUE_MAX] = {};
         if (property_get("ro.boot.product.vendor.sku", value, "") <= 0) {
             return std::vector<std::string>({"/odm/etc", "/vendor/etc/audio",
-                    "/vendor/etc", "/system/etc"});
+                    "/vendor/etc/audio_ar", "/vendor/etc", "/system/etc"});
         } else {
             return std::vector<std::string>({
                     "/odm/etc", std::string("/vendor/etc/audio/sku_") + value,
-                    "/vendor/etc/audio", "/vendor/etc", "/system/etc"});
+                    "/vendor/etc/audio", "/vendor/etc/audio_ar", "/vendor/etc",
+                    "/system/etc"});
         }
     }();
     return paths;
